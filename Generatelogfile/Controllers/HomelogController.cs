3da7dff146ce019b99605a25f9e04a07b87e5334
Sameer Khaire <Sameer.Khaire@rishabhsoft.com>
2023-09-27 22:56:49 +0530

    Add project files.

---
+++ Generatelogfile/Controllers/HomelogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Generatelogfile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomelogController : ControllerBase
+    {
+        private readonly ILogger<HomelogController> _logger;
+        public HomelogController(ILogger<HomelogController> logger)
+        {
+            _logger=logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogTrace("logger trace method");
+            _logger.LogWarning("Logger warning");
+            _logger.LogDebug("debugggg");
+            _logger.LogInformation("Information");
+            return Ok();
+        }
+    }
+}
